aac7e72469239d2d89d3d5cb5464d6acdbe61eaf
Caique <asapcaique7@gmail.com>
2024-02-15 02:12:15 -0300

    navbar responsivo

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -2,16 +2,12 @@ import DeicideLogo from "../../../../public/assets/img/deicide-logo.png"
 import Image from "next/image";
 import "./Header.css";
 import Navbar from "../navbar/Navbar";
-import { GiCharacter } from "react-icons/gi";
 
 export default function Header() {
     return(
-        <header className="w-full flex justify-between items-center">
-            <Image className="w-[70px] h-[90px] ml-6" src={DeicideLogo}/>
+        <header className="w-full flex items-center">
+            <Image className="w-[70px] h-[90px] ml-6 self-start" src={DeicideLogo}/>
             <Navbar/>
-            <button className="login-btn mr-3 flex gap-2 items-center justify-center h-[40px] pl-2 pr-2 
-            rounded-xl text-2xl">
-                <GiCharacter size={24} />Login</button>
         </header>
     )
 }
--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,11 +1,88 @@
+"use client";
+import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
+import "./Navbar.css"
+import { useState, useEffect } from "react";
+
 export default function Navbar(){
+    const [isActive, setIsActive] = useState(false);
+
+    const sideList = [
+        {
+          title: 'Home',
+          path: '/',
+        },
+        {
+          title: 'Playlist',
+          path: '/',
+        },
+        {
+          title: 'Sobre',
+          path: '/about',
+        },
+      ];
+
+    const toggleMenu = () => {
+        setIsActive(!isActive);
+    }
+
+    useEffect(() => {
+        const handleEscKeyPress = (e) => {
+            if (e.keycode === 27 && isActive) {
+                setIsActive(false);
+            }
+        };
+
+        if (isActive) {
+            document.body.style.setProperty("overflow", "hidden");
+        }else {
+            document.body.style.setProperty("overflow", "auto");
+        }
+
+        document.addEventListener("keydown", handleEscKeyPress);
+        
+        return () => {
+            document.removeEventListener("keydown", handleEscKeyPress);
+        };
+    }, [isActive]);
+    
     return(
-        <nav className="flex items-center justify-center">
-            <ul className="flex gap-4 font-bold text-xl">
+        <nav className="flex items-center justify-center self-center ml-[20%] majortwomin:ml-[25%] majorthree:ml-[16%]
+  majorfour1:ml-[10%] midtwo4:ml-[5%] ">
+
+            <ul className="flex gap-4 font-bold text-2xl midthree:text-lg">
                 <li>Home</li>
                 <li>Playlist</li>
                 <li>Informações/Contato</li>
             </ul>
+            <button aria-label="Open Menu" onClick={toggleMenu} className="hidden self-end ml-[47%]">
+                <GiHamburgerMenu size={60} className="" style={{ color: 'black' }} />
+</button>
+
+                {isActive && (
+        <div className="z-10 fixed inset-0 transition-opacity">
+          <div
+            onClick={() => setIsActive(false)}
+            className="absolute inset-0 bg-black opacity-50"
+            tabIndex="0"
+          ></div>
+        </div>
+      )}
+
+      <aside
+        className={`transform top-0 left-0 w-64 lowtwo2:w-44 lowthreetwo:w-36 text-white font-extrabold fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30 ${
+          isActive ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+         
+         {sideList.map(({ title, path }, index) => (
+    <Link href={path} key={index}>
+        <span className="flex items-center p-4 hover:bg-blue-600 hover:text-orange-400">
+          <span className="border-b-4">{title}</span>
+        </span>
+    </Link>
+  ))}
+      </aside>
         </nav>
-    )
+    );
 }
